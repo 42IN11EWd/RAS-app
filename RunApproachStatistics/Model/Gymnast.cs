c7062d32e4290e20184be0c48d3c83ae08759c56
Sjoerd Nijhof <sjoerdnijhof@hotmail.com>
2014-04-17 10:41:06 +0200

    Added enum class for the genders, Added vault class

--- RunApproachStatistics/Model/Gymnast.cs
+++ RunApproachStatistics/Model/Gymnast.cs
@@ -8,5 +8,28 @@ namespace RunApproachStatistics.Model
 {
     public class Gymnast
     {
+        // Properties
+        public int Id { get; set; }
+        public long GymnasticsFederationId { get; set; }
+        public GenderEnum Gender { get; set; }
+        public String Nationality { get; set; }
+        public int Length { get; set; }
+        public DateTime Birthday { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String SurnamePrefix { get; set; }
+        public List<Vault> VaultCollection { get; set; }
+
+        /// <summary>
+        /// This module is responsible for the......
+        /// ........................................
+        /// ........................................
+        /// ........................................
+        /// </summary>
+        public Gymnast(int id, long gymnasticFederationId, GenderEnum gender, String nationality, int length, DateTime birthday, String name, 
+                       String surname, String surnamePrefix, List<Vault> vaultCollection)
+        {
+
+        }
     }
 }
