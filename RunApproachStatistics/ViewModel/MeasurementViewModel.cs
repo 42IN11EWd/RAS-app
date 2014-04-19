9ba770423783f1a48f2b812096929209a3308492
Stefanie van de Poel <sjpoel@avans.nl>
2014-04-19 13:14:46 +0200

    togglebutton rating fix

--- RunApproachStatistics/ViewModel/MeasurementViewModel.cs
+++ RunApproachStatistics/ViewModel/MeasurementViewModel.cs
@@ -3,17 +3,19 @@ using RunApproachStatistics.Modules;
 using RunApproachStatistics.Modules.Interfaces;
 using RunApproachStatistics.MVVM;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RunApproachStatistics.ViewModel
 {
     public class MeasurementViewModel : AbstractViewModel
     {
         private IApplicationController _app;
-        private PropertyChangedBase ratingValue;
+        private int ratingValue;
+        private bool star1Checked;
+        private bool star2Checked;
+        private bool star3Checked;
+        private bool star4Checked;
+        private bool star5Checked;
+        private bool[] starArray = new bool[6];
 
         #region Modules
 
@@ -24,15 +26,89 @@ namespace RunApproachStatistics.ViewModel
         #region DataBinding
 
         public RelayCommand PostMeasurementCommand { get; private set; }
-        public RelayCommand ClickRatingCommand { get; private set; }
 
-        public PropertyChangedBase RatingValue
+        public int RatingValue
         {
             get { return ratingValue; }
             set
             {
                 ratingValue = value;
+                Console.WriteLine(ratingValue);
+                for (int i = 1; i < 6; i++ )
+                {
+                    
+                    if(i <= ratingValue)
+                    {
+                        starArray[i] = true;
+                    }
+                    else
+                    {
+                        starArray[i] = false;
+                    }
+                }
+                star1Checked = starArray[1];
+                star2Checked = starArray[2];
+                star3Checked = starArray[3];
+                star4Checked = starArray[4];
+                star5Checked = starArray[5];
+                OnPropertyChanged("Star1Checked");
+                OnPropertyChanged("Star2Checked");
+                OnPropertyChanged("Star3Checked");
+                OnPropertyChanged("Star4Checked");
+                OnPropertyChanged("Star5Checked");
                 OnPropertyChanged("RatingValue");
+            }
+        }
+        public bool Star1Checked
+        {
+            get { return star1Checked; }
+            set
+            {
+                star1Checked = value;
+                RatingValue = 1;
+                OnPropertyChanged("Star1Checked");
+            }
+        }
+        public bool Star2Checked
+        {
+            get { return star2Checked; }
+            set
+            {
+                star2Checked = value;
+                RatingValue = 2;
+                OnPropertyChanged("Star2Checked");
+                
+            }
+        }
+        public bool Star3Checked
+        {
+            get { return star3Checked; }
+            set
+            {
+                star3Checked = value;
+                RatingValue = 3;
+                OnPropertyChanged("Star3Checked");
+            }
+        }
+        public bool Star4Checked
+        {
+            get { return star4Checked; }
+            set
+            {
+                star4Checked = value;
+                RatingValue = 4;
+                OnPropertyChanged("Star4Checked");
+                
+            }
+        }
+        public bool Star5Checked
+        {
+            get { return star5Checked; }
+            set
+            {
+                star5Checked = value;
+                RatingValue = 5;
+                OnPropertyChanged("Star5Checked");
             }
         }
 
@@ -49,19 +125,13 @@ namespace RunApproachStatistics.ViewModel
         {
             _app.ShowPostMeasurementView();
         }
-        public void ClickRating(object commandParam)
-        {
-
-            //Console.WriteLine("clicked: " + commandParam.ToString());
-            Console.WriteLine("RatingValue: " + RatingValue);
-        }
 
         #endregion
 
         protected override void initRelayCommands()
         {
             PostMeasurementCommand = new RelayCommand(LoadPostMeasurementScreen);
-            ClickRatingCommand = new RelayCommand(ClickRating);
         }
+       
     }
 }
