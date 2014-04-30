94ddecbc9eda9baf1fe760ed2108a618b79be6c0
Stefanie van de Poel <sjpoel@avans.nl>
2014-04-30 12:57:50 +0200

    editors almost working. only database todo

--- RunApproachStatistics/ViewModel/LocationEditorViewModel.cs
+++ RunApproachStatistics/ViewModel/LocationEditorViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using RunApproachStatistics.Controllers;
+using RunApproachStatistics.Model.Entity;
 using RunApproachStatistics.MVVM;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,14 @@ namespace RunApproachStatistics.ViewModel
     {
         private IApplicationController _app;
         private PropertyChangedBase content;
+        private ObservableCollection<location> locations = new ObservableCollection<location>();
+        private location selectedItem;        
 
         #region Bindings
-
+        public RelayCommand DeleteCommand { get; private set; }
+        public RelayCommand NewCommand { get; private set; }
+        public RelayCommand SaveCommand { get; private set; }
+        public RelayCommand BackCommand { get; private set; }
         public PropertyChangedBase Content
         {
             get { return content; }
@@ -24,17 +31,103 @@ namespace RunApproachStatistics.ViewModel
                 OnPropertyChanged("Content");
             }
         }
+        public ObservableCollection<location> Locations
+        {
+            get { return locations; }
+            set
+            {
+                locations = value;
+                this.OnPropertyChanged("Locations");
+            }
+        }
+        public location SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+                OnPropertyChanged("Name");
+                OnPropertyChanged("Description");
+                
+            }
+        }
+        public String Name
+        {
+            get {
+                if (SelectedItem != null)
+                    return SelectedItem.name;
+                return "";
+                
+            }
+            set 
+            { 
+                SelectedItem.name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public String Description
+        {
+            get
+            {
+                if (SelectedItem != null)
+                    return SelectedItem.description;
+                return "";
+            }
+            set 
+            {
+                SelectedItem.description = value;
+                OnPropertyChanged("Description");
+            }
+        }
 
         #endregion
 
         public LocationEditorViewModel(IApplicationController app) : base()
         {
             _app = app;
+            //fill locations list
+            locations.Add(new location{
+                name = "Flikflak",
+                description = "flikflak hal in den bosch"
+            });
+            locations.Add(new location
+            {
+                name = "Eindhoven",
+                description = "gymnastiek hal in eindhoven"
+            });
+
         }
+        #region RelayCommands
+
+        public void DeleteAction(object commandParam)
+        {
+            SelectedItem.deleted = true;
+            locations.Remove(SelectedItem);
+        }
+        public void NewAction(object commandParam)
+        {
+            //new location
+            location newlocation = new location();
+            locations.Add(newlocation);
+            SelectedItem = newlocation;
+        }
+        public void SaveAction(object commandParam)
+        {
+            //save changes
+        }
+        public void BackAction(object commandParam)
+        {
+            _app.ShowSettingsView();
+        }
+        #endregion
 
         protected override void initRelayCommands()
         {
-            
+            DeleteCommand = new RelayCommand(DeleteAction);
+            NewCommand = new RelayCommand(NewAction);
+            SaveCommand = new RelayCommand(SaveAction);
+            BackCommand = new RelayCommand(BackAction);
         }
     }
 }
--- RunApproachStatistics/ViewModel/VaultNumberEditorViewModel.cs
+++ RunApproachStatistics/ViewModel/VaultNumberEditorViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using RunApproachStatistics.Controllers;
+using RunApproachStatistics.Model.Entity;
 using RunApproachStatistics.MVVM;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,16 @@ namespace RunApproachStatistics.ViewModel
 {
     class VaultNumberEditorViewModel : AbstractViewModel
     {
-   private IApplicationController _app;
+        private IApplicationController _app;
         private PropertyChangedBase content;
+        private ObservableCollection<vaultnumber> vaults = new ObservableCollection<vaultnumber>();
+        private vaultnumber selectedItem;
 
         #region Bindings
+        public RelayCommand DeleteCommand { get; private set; }
+        public RelayCommand NewCommand { get; private set; }
+        public RelayCommand SaveCommand { get; private set; }
+        public RelayCommand BackCommand { get; private set; }
 
         public PropertyChangedBase Content
         {
@@ -24,17 +32,137 @@ namespace RunApproachStatistics.ViewModel
                 OnPropertyChanged("Content");
             }
         }
+        public ObservableCollection<vaultnumber> Vaults
+        {
+            get { return vaults; }
+            set
+            {
+                vaults = value;
+                this.OnPropertyChanged("Locations");
+            }
+        }
+        public vaultnumber SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+                OnPropertyChanged("Code");
+                OnPropertyChanged("Gender");
+                OnPropertyChanged("Difficulty");
+                OnPropertyChanged("Description");
+
+            }
+        }
+        public String Code
+        {
+            get
+            {
+                if (SelectedItem != null)
+                    return SelectedItem.code;
+                return "";
+
+            }
+            set
+            {
+                SelectedItem.code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+        public String Gender
+        {
+            get
+            {
+                if (SelectedItem != null)
+                    return SelectedItem.male_female;
+                return "";
+
+            }
+            set
+            {
+                SelectedItem.male_female = value;
+                OnPropertyChanged("Gender");
+            }
+        }
+        public Nullable<decimal> Difficulty
+        {
+            get
+            {
+                if (SelectedItem != null)
+                    return SelectedItem.difficulty;
+                return 0;
+
+            }
+            set
+            {
+                SelectedItem.difficulty = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public String Description
+        {
+            get
+            {
+                if (SelectedItem != null)
+                    return SelectedItem.description;
+                return "";
+            }
+            set
+            {
+                SelectedItem.description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
 
         #endregion
 
         public VaultNumberEditorViewModel(IApplicationController app) : base()
         {
             _app = app;
+            //fill vaults list
+            vaults.Add(new vaultnumber
+            {
+                code = "30.1",
+                description = "flikflak hal in den bosch"
+            });
+            vaults.Add(new vaultnumber
+            {
+                code = "5.12",
+                description = "gymnastiek hal in eindhoven"
+            });
         }
+
+        #region RelayCommands
+
+        public void DeleteAction(object commandParam)
+        {
+            SelectedItem.deleted = true;
+            vaults.Remove(SelectedItem);
+        }
+        public void NewAction(object commandParam)
+        {
+            vaultnumber newvault = new vaultnumber();
+            vaults.Add(newvault);
+            SelectedItem = newvault;
+        }
+        public void SaveAction(object commandParam)
+        {
+            //save changes 
+        }
+        public void BackAction(object commandParam)
+        {
+            _app.ShowSettingsView();
+        }
+        #endregion
 
         protected override void initRelayCommands()
         {
-            
+            DeleteCommand = new RelayCommand(DeleteAction);
+            NewCommand = new RelayCommand(NewAction);
+            SaveCommand = new RelayCommand(SaveAction);
+            BackCommand = new RelayCommand(BackAction);
         }
     }
 }
