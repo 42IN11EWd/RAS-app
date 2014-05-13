14e9912602191e1f8805e5532b9e56915b6f6176
Alex Nagelkerke <alex_na@outlook.com>
2014-05-13 13:01:17 +0200

    [IN-25] Ensure item in List validation added

---
+++ RunApproachStatistics/MVVM/Validation/EnsureInList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunApproachStatistics.MVVM.Validation
+{
+    class EnsureInList : ValidationAttribute
+    {
+        private readonly IList _listToCheck;
+        public EnsureInList(IList listToCheck)
+        {
+            _listToCheck = listToCheck;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return _listToCheck.Contains(value);
+        }
+    }
+}
