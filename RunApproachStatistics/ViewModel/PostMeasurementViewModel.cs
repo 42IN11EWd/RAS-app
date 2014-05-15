dc7bef16e5b0a36185507b22115c4cdcf52e6c81
Stefan van der Pas <stefan10pas@hotmail.com>
2014-05-15 12:40:42 +0200

    set ratingviewmodel to get the amount of stars from thumbnail

--- RunApproachStatistics/ViewModel/PostMeasurementViewModel.cs
+++ RunApproachStatistics/ViewModel/PostMeasurementViewModel.cs
@@ -97,7 +97,6 @@ namespace RunApproachStatistics.ViewModel
             {
                 selectedThumbnail = value;
                 setScores();
-                //OnPropertyChanged("StarRating");
                 ratingVM.RatingValue = (int)SelectedThumbnail.Vault.rating_star;
                 OnPropertyChanged("SelectedThumbnail");
                 OnPropertyChanged("Gymnast");
@@ -111,23 +110,23 @@ namespace RunApproachStatistics.ViewModel
         }
 
 
-        public int StarRating
-        {
-            get
-            {
-                if (SelectedThumbnail != null)
-                {
-                    ratingVM.RatingValue = (int)SelectedThumbnail.Vault.rating_star;
-                    return (int)SelectedThumbnail.Vault.rating_star;
-                }
-                return 0;
-            }
-            set
-            {
-                SelectedThumbnail.Vault.rating_star = ratingVM.RatingValue;
-                OnPropertyChanged("StarRating");
-            }
-        }
+        //public int StarRating
+        //{
+        //    get
+        //    {
+        //        if (SelectedThumbnail != null)
+        //        {
+        //            ratingVM.RatingValue = (int)SelectedThumbnail.Vault.rating_star;
+        //            return (int)SelectedThumbnail.Vault.rating_star;
+        //        }
+        //        return 0;
+        //    }
+        //    set
+        //    {
+        //        SelectedThumbnail.Vault.rating_star = ratingVM.RatingValue;
+        //        OnPropertyChanged("StarRating");
+        //    }
+        //}
 
         public String Gymnast
         {
@@ -315,8 +314,9 @@ namespace RunApproachStatistics.ViewModel
             menuViewModel.VisibilityLaser = false;
             Menu = menuViewModel;
 
-            RatingViewModel ratingVM = new RatingViewModel(_app);
+            ratingVM = new RatingViewModel(_app);
             RatingControl = ratingVM;
+
             // Useless test data.
             thumbnailCollection = new ObservableCollection<ThumbnailViewModel>();
             List<vault> vaults = vaultModule.getVaults();
