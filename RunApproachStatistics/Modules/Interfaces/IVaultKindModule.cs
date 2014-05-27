98946032401bfba053001c6a6a51c7a6b109b9d2
Tim Manders <t__manders@hotmail.com>
2014-05-27 22:09:13 +0200

    Created IVaultKindModule. Implemented IVaultKindModule in EditorModule. Created VaultKindEditorViewModel supporting all actions needed for the view to work.

---
+++ RunApproachStatistics/Modules/Interfaces/IVaultKindModule.cs
@@ -0,0 +1,18 @@
+﻿using RunApproachStatistics.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunApproachStatistics.Modules.Interfaces
+{
+    interface IVaultKindModule
+    {
+        void deleteVaultKind(int id);
+        ObservableCollection<vaultkind> readVaultKinds();
+        void createVaultKind(vaultkind vaultKind);
+        void updateVaultKind(vaultkind vaultKind);
+    }
+}
